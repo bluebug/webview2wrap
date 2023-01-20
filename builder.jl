1bf3c1470a35c78fd2f28ef7cdbd91fe9b9e11b6
bluebug <bluebug.cn@gmail.com>
2023-01-20 21:56:01 +0800

    create binary icon

--- builder.jl
+++ builder.jl
@@ -1,6 +1,11 @@
+run(`windres -i ./icons/icon.rc ./icons/icon.o`)
+
+import Pkg;
+Pkg.add("PackageCompiler");
+
 using PackageCompiler
 
-ENV["JULIA_CC"] = "gcc -lole32 -lshell32 -lshlwapi -luser32 -lgdi32 -mwindows"
+ENV["JULIA_CC"] = "gcc ./icons/icon.o -lole32 -lshell32 -lshlwapi -luser32 -lgdi32 -mwindows"
 
 create_app(".", "dist"; force=true)
 
